f57ad2ac297301aa225bf03ec879f88a150c06ed
Anna Luchechko <annaluchechko@gmail.com>
2020-07-03 16:02:53 +0300

    Create clean project

---
+++ vkClient/vkClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vkClient
+//
+//  Created by Anna Luchechko on 03.07.2020.
+//  Copyright © 2020 Anna Luchechko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
