9175a07d59d47f47a79815fedb14e210015aa1f5
Anna Luchechko <annaluchechko@gmail.com>
2020-07-07 14:23:11 +0300

    Refactor ViewController

--- vkClient/vkClient/ViewController.swift
+++ vkClient/vkClient/ViewController.swift
@@ -25,20 +25,17 @@ class ViewController: UIViewController {
     }
     
     @objc func keyboardWasShown(notification: Notification) {
-           // Получаем размер клавиатуры
-           let info = notification.userInfo! as NSDictionary
-           let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
-           let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
-           
-           // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
-           self.scrollView?.contentInset = contentInsets
-           scrollView?.scrollIndicatorInsets = contentInsets
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+
+        self.scrollView?.contentInset = contentInsets
+        self.scrollView?.scrollIndicatorInsets = contentInsets
     }
        
     @objc func keyboardWillBeHidden(notification: Notification) {
-           // Устанавливаем отступ внизу UIScrollView, равный 0
-           let contentInsets = UIEdgeInsets.zero
-           scrollView?.contentInset = contentInsets
+        let contentInsets = UIEdgeInsets.zero
+        self.scrollView?.contentInset = contentInsets
     }
     
     override func viewWillDisappear(_ animated: Bool) {
