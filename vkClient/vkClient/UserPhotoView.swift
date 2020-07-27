bf3d9bca9b096587f43c04fd403b6815fdb119ba
Anna Luchechko <annaluchechko@gmail.com>
2020-07-27 22:53:52 +0300

    Add shadowSubview and subview for image

--- vkClient/vkClient/UserPhotoView.swift
+++ vkClient/vkClient/UserPhotoView.swift
@@ -7,3 +7,41 @@
 //
 
 import UIKit
+
+@IBDesignable class UserPhotoView: UIView {
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let shadowSubview = UIView()
+        let borderView = UIView()
+
+        shadowSubview.frame.size = CGSize(width: 200, height: 200)
+        shadowSubview.backgroundColor = UIColor.clear
+        shadowSubview.layer.shadowColor = UIColor.black.cgColor
+        shadowSubview.layer.shadowOffset = CGSize(width: 3, height: 3)
+        shadowSubview.layer.shadowOpacity = 0.7
+        shadowSubview.layer.shadowRadius = 4.0
+        shadowSubview.layer.shadowPath = UIBezierPath(roundedRect: shadowSubview.bounds, cornerRadius: 10).cgPath
+        shadowSubview.layer.shouldRasterize = true
+        shadowSubview.layer.rasterizationScale = UIScreen.main.scale
+
+        borderView.frame = shadowSubview.bounds
+        borderView.layer.cornerRadius = 10
+        borderView.layer.borderColor = UIColor.black.cgColor
+        borderView.layer.borderWidth = 1.0
+        borderView.layer.masksToBounds = true
+        shadowSubview.addSubview(borderView)
+
+        let otherSubContent = UIImageView()
+        otherSubContent.image = UIImage(named: "cars")
+        otherSubContent.contentMode = .scaleToFill
+        otherSubContent.frame = borderView.bounds
+        otherSubContent.layer.masksToBounds = true
+        borderView.addSubview(otherSubContent)
+
+        addSubview(shadowSubview)
+
+    }
+    
+}
