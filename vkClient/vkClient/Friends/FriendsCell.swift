2e9847a32b4488e1d914b26e471b7f3093abcb14
Anna Luchechko <annaluchechko@gmail.com>
2020-09-30 16:39:17 +0300

    Remove UserPhotoView

--- vkClient/vkClient/Friends/FriendsCell.swift
+++ vkClient/vkClient/Friends/FriendsCell.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class FriendsCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var friendimage: UserPhotoView!
+    @IBOutlet weak var containerFriendImage: UIView!
+    @IBOutlet weak var friendimage: UIImageView!
     
     override func prepareForReuse() {
         super.prepareForReuse()
         
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        containerFriendImage.layer.shadowColor = UIColor.black.cgColor
+        containerFriendImage.layer.cornerRadius = containerFriendImage.bounds.width / 2
+        containerFriendImage.layer.shadowRadius = 10
+        containerFriendImage.layer.shadowOpacity = 0.5
+        containerFriendImage.layer.shadowOffset = .zero
+        containerFriendImage.layer.shadowPath = UIBezierPath(ovalIn: containerFriendImage.bounds).cgPath
+        
+        friendimage.layer.cornerRadius = containerFriendImage.bounds.width / 2
+        friendimage.clipsToBounds = true
     }
 }
