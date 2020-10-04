1e7a4b7e7f40d5efedf13ba536914fb876452816
Anna Luchechko <annaluchechko@gmail.com>
2020-10-05 02:46:12 +0300

    Fix animation on DotsLoadingIndicator

--- vkClient/vkClient/DotsLoadingIndicator.swift
+++ vkClient/vkClient/DotsLoadingIndicator.swift
@@ -9,16 +9,11 @@ import UIKit
 
 class DotsLoadingIndicator: UIView {
     
-    private var firstDot: UIView = UIView()
-    private var secondDot: UIView = UIView()
-    private var thirdDot: UIView = UIView()
-    
-    
     //called when initialized programmatically
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addDots()
-        dotsLoadingAnimation()
+        
+        addDots(dotsCount: 3)
         
     }
     
@@ -26,40 +21,29 @@ class DotsLoadingIndicator: UIView {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
 
-        addDots()
-        dotsLoadingAnimation()
-
-    }
-    
-    func addDots() {
-        firstDot.frame = CGRect(x: frame.width * 0.2 + 15, y: frame.height/2 - 15, width: 30, height: 30)
-        firstDot.backgroundColor = .black
-        firstDot.layer.cornerRadius = 15
-
-        secondDot.frame = CGRect(x: frame.width * 0.4 + 15, y: frame.height/2 - 15, width: 30, height: 30)
-        secondDot.backgroundColor = .black
-        secondDot.layer.cornerRadius = 15
-
-
-        thirdDot.frame = CGRect(x: frame.width * 0.6 + 15, y: frame.height/2 - 15, width: 30, height: 30)
-        thirdDot.backgroundColor = .black
-        thirdDot.layer.cornerRadius = 15
-        
-        addSubview(firstDot)
-        addSubview(secondDot)
-        addSubview(thirdDot)
+        addDots(dotsCount: 3)
         
     }
     
-    func dotsLoadingAnimation() {
-        UIView.animate(withDuration: 1.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
-            self.firstDot.alpha = 0.3
-            self.secondDot.alpha = 0.5
-            self.thirdDot.alpha = 0.8
-            self.firstDot.alpha = 0.8
-            self.secondDot.alpha = 0.3
-            self.thirdDot.alpha = 0.5
+    func addDots(dotsCount: Int) {
+        var dot = 0
         
+        while dot < dotsCount {
+            let dotView = UIView(frame: CGRect(x: (frame.width * 0.2 * ((CGFloat(dot) + 1)) + 15),
+                                               y: frame.height/2 - 15, width: 30, height: 30))
+            dotView.layer.cornerRadius = 15
+            addSubview(dotView)
+            dotView.backgroundColor = .gray
+            dotsLoadingAnimation(view: dotView, delay: Double(dot)/3)
+            dot += 1
+        }
+                
+    }
+    
+    func dotsLoadingAnimation(view: UIView, delay: Double) {
+        UIView.animate(withDuration: 1.0, delay: delay, options: [.repeat, .autoreverse], animations: {
+            view.alpha = 1.0
+            view.alpha = 0.0
         })
         
         
