f3d5c7ee77d8d0b2b0f8439df9a8c526d49faa9c
Anna Luchechko <annaluchechko@gmail.com>
2020-10-08 02:59:04 +0300

    Add swipe animation to FriendsGalleryController

--- vkClient/vkClient/Friends/FriendsGalleryController.swift
+++ vkClient/vkClient/Friends/FriendsGalleryController.swift
@@ -11,40 +11,141 @@ import UIKit
 class FriendsGalleryController: UIViewController {
     
     var photosArray = [String]()
-    let galleryScrollView = UIScrollView()
+    var currentIndex: Int = 0
+    private var currentSign = 0
+    private var percent: CGFloat = 0
+    
+    private var interactiveAnimator: UIViewPropertyAnimator?
+        
+    private let imageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleToFill
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+    
+    private let backgroundImageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleToFill
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        galleryScrollView.isPagingEnabled = true
-        galleryScrollView.showsVerticalScrollIndicator = false
-        galleryScrollView.contentInsetAdjustmentBehavior = .never
-        galleryScrollView.showsHorizontalScrollIndicator = false
-        galleryScrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        view.backgroundColor = .white
+        layout(imgView: backgroundImageView)
+        layout(imgView: imageView)
+        setImages()
         
-        view.addSubview(galleryScrollView)
-        setupImages(photosArray)
-        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(onPan(_:)))
+        imageView.addGestureRecognizer(gesture)
+    
     }
     
-    func setupImages(_ images: [String]){
-        for i in 0..<images.count {
-
-            let imageView = UIView(frame:  CGRect(x: 0, y: 0, width: galleryScrollView.frame.width, height: galleryScrollView.frame.height))
-            let image = UIImageView()
-
-            image.frame = imageView.bounds
-            image.frame.origin.x = view.frame.width * CGFloat(i)
-            image.image = UIImage(named: images[i])
-            image.contentMode = .scaleAspectFit
-            
-            imageView.addSubview(image)
-
-            galleryScrollView.contentSize.width = galleryScrollView.frame.width * CGFloat(i + 1)
-            galleryScrollView.contentSize.height = view.frame.height
-            galleryScrollView.addSubview(imageView)
-            
-        }
+    private func layout(imgView: UIImageView) {
+        view.addSubview(imgView)
+        NSLayoutConstraint.activate([
+            imgView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imgView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imgView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            imgView.heightAnchor.constraint(equalTo: view.widthAnchor)
+        ])
     }
 
+    private func setImages() {
+        let firstImage = UIImage(named: photosArray[currentIndex])
+        var nextIndex = currentIndex + 1
+        var backgroundImage: UIImage?
+        
+        if currentSign > 0 {
+            nextIndex = currentIndex - 1
+        }
+        
+        if nextIndex < photosArray.count - 1, nextIndex >= 0 {
+            backgroundImage = UIImage(named: photosArray[nextIndex])
+        }
+        
+        imageView.image = firstImage
+        backgroundImageView.image = backgroundImage
+
+    }
+    
+    private func initAnimator() {
+        backgroundImageView.alpha = 0.0
+        backgroundImageView.transform = .init(scaleX: 0.8, y: 0.8)
+        interactiveAnimator?.stopAnimation(true)
+        interactiveAnimator = UIViewPropertyAnimator(duration: 0.5,
+            curve: .easeInOut, animations: {
+                let width = CGFloat(self.currentSign) * self.view.frame.width
+                let translationTransform = CGAffineTransform(translationX: width, y: 0)
+                let anggle = CGFloat(self.currentSign) * 0.8
+                
+                let angleTransform = CGAffineTransform(rotationAngle: anggle)
+                self.imageView.transform = angleTransform.concatenating(translationTransform)
+                
+                self.backgroundImageView.alpha = 1.0
+                self.backgroundImageView.transform = .identity
+            })
+        interactiveAnimator?.startAnimation()
+        interactiveAnimator?.pauseAnimation()
+    }
+    
+    private func resetImageView() {
+        backgroundImageView.alpha = 0.0
+        backgroundImageView.transform = .init(scaleX: 0.8, y: 0.8)
+        imageView.transform = .identity
+        
+        setImages()
+        view.layoutIfNeeded()
+        currentSign = 0
+        interactiveAnimator = nil
+    }
+    
+    @objc private func onPan(_ gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            let translation = gesture.translation(in: view)
+            percent = abs(translation.x) / view.frame.width
+            let translationX = Int(translation.x)
+            let sign = translationX == 0 ? 1 : translationX / abs(translationX)
+            
+            if interactiveAnimator == nil || sign != currentSign {
+                interactiveAnimator?.stopAnimation(true)
+                resetImageView()
+                interactiveAnimator = nil
+                
+                if (sign > 0 && currentIndex > 0 || (sign < 0 && currentIndex < photosArray.count - 1)) {
+                    currentSign = sign
+                    setImages()
+                    initAnimator()
+                }
+            }
+            
+            interactiveAnimator?.fractionComplete = abs(translation.x) / (self.view.frame.width / 2)
+            
+        case .ended:
+            interactiveAnimator?.addCompletion( { position in
+                self.resetImageView()
+            })
+            
+            if percent < 0.33 {
+                interactiveAnimator?.stopAnimation(true)
+                UIView.animate(withDuration: 0.3) {
+                    self.resetImageView()
+                }
+            } else {
+                currentIndex += currentSign * -1
+                interactiveAnimator?.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+            }
+            
+        default:
+            break
+        }
+    }
+    
 }
