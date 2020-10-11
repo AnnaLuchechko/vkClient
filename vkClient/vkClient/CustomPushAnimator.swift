3752b7579ec4f60fba8f9249522066f543186ec9
Anna Luchechko <annaluchechko@gmail.com>
2020-10-11 20:51:46 +0300

    Add CustomPushAnimator

---
+++ vkClient/vkClient/CustomPushAnimator.swift
@@ -0,0 +1,83 @@
+//
+//  CustomPushAnimator.swift
+//  vkClient
+//
+//  Created by Anna Luchechko on 11.10.2020.
+//  Copyright © 2020 Anna Luchechko. All rights reserved.
+//
+
+import UIKit
+
+class CustomPushAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.6
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let source = transitionContext.viewController(forKey: .from) else { return }
+        guard let destination = transitionContext.viewController(forKey: .to) else { return }
+
+        
+        transitionContext.containerView.addSubview(destination.view)
+        destination.view.frame = source.view.frame
+        destination.view.transform = CGAffineTransform(translationX: source.view.frame.width, y: 0)
+        
+        
+        UIView.addKeyframe(withRelativeStartTime: 0,
+                           relativeDuration: 0.75,
+                           animations: {
+                               let translation = CGAffineTransform(translationX: -200, y: 0)
+                               let scale = CGAffineTransform(scaleX: 0.8, y: 0.8)
+                               source.view.transform = translation.concatenating(scale)
+        })
+
+        
+        UIView.addKeyframe(withRelativeStartTime: 0.2,
+                           relativeDuration: 0.4,
+                           animations: {
+                               let translation = CGAffineTransform(translationX: source.view.frame.width / 2, y: 0)
+                               let scale = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                               destination.view.transform = translation.concatenating(scale)
+        })
+        
+        UIView.addKeyframe(withRelativeStartTime: 0.6,
+                           relativeDuration: 0.4,
+                           animations: {
+                               destination.view.transform = .identity
+        })
+        
+        UIView.animateKeyframes(withDuration: self.transitionDuration(using: transitionContext),
+                                delay: 0,
+                                options: .calculationModePaced,
+                                animations: {
+                                    UIView.addKeyframe(withRelativeStartTime: 0,
+                                                       relativeDuration: 0.75,
+                                                       animations: {
+                                                           let translation = CGAffineTransform(translationX: -200, y: 0)
+                                                           let scale = CGAffineTransform(scaleX: 0.8, y: 0.8)
+                                                           source.view.transform = translation.concatenating(scale)
+                                    })
+                                    UIView.addKeyframe(withRelativeStartTime: 0.2,
+                                                       relativeDuration: 0.4,
+                                                       animations: {
+                                                           let translation = CGAffineTransform(translationX: source.view.frame.width / 2, y: 0)
+                                                           let scale = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                                                           destination.view.transform = translation.concatenating(scale)
+                                    })
+                                    UIView.addKeyframe(withRelativeStartTime: 0.6,
+                                                       relativeDuration: 0.4,
+                                                       animations: {
+                                                           destination.view.transform = .identity
+                                    })
+        }) { finished in
+            if finished && !transitionContext.transitionWasCancelled {
+                source.view.transform = .identity
+            }
+            transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
+        }
+
+    }
+    
+}
+
