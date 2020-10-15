7fc03c583b3ae97821aced57156a456a625387fa
Anna Luchechko <annaluchechko@gmail.com>
2020-10-15 13:52:27 +0300

    Add request types to VKNetworkService

--- vkClient/Manager/VKNetworkService.swift
+++ vkClient/Manager/VKNetworkService.swift
@@ -10,8 +10,16 @@ import Foundation
 
 class VKNetworkService {
     
-    func getData(token:String, userID: Int) {
-        // Custom session
+    // List of request type to API
+    enum VKParameters {
+        case friendsList
+        case userPhotos
+        case userGroups
+        case searchGroups
+    }
+    
+    // Custom session
+    func getData(token:String, userID: Int, vkParameters: VKParameters) {
         let configuration = URLSessionConfiguration.default
         let session =  URLSession(configuration: configuration)
         
@@ -20,14 +28,33 @@ class VKNetworkService {
         urlConstructor.host = "api.vk.com"
         urlConstructor.path = "/method/users.get"
         urlConstructor.queryItems = [
-            URLQueryItem(name: "user_ids", value: String(userID)),
-            URLQueryItem(name: "fields", value: "bdate"),
             URLQueryItem(name: "access_token", value: token),
             URLQueryItem(name: "v", value: "5.124"),
         ]
         
+        // Build URL-parameters to get requested data
+        switch vkParameters {
+        case .friendsList:
+            urlConstructor.path = "/method/friends.get"
+            urlConstructor.queryItems?.append(URLQueryItem(name: "user_id", value: String(userID)))
+            urlConstructor.queryItems?.append(URLQueryItem(name: "fields", value: "photo_50"))
+        case .userPhotos:
+            urlConstructor.path = "/method/photos.getAll"
+            urlConstructor.queryItems?.append(URLQueryItem(name: "owner_id", value: String(userID)))
+        case .userGroups:
+            urlConstructor.path = "/method/groups.get"
+            urlConstructor.queryItems?.append(URLQueryItem(name: "user_id", value: String(userID)))
+            urlConstructor.queryItems?.append(URLQueryItem(name: "extended", value: "1"))
+        case .searchGroups:
+            urlConstructor.path = "/method/groups.search"
+            urlConstructor.queryItems?.append(URLQueryItem(name: "q", value: "video"))
+            urlConstructor.queryItems?.append(URLQueryItem(name: "type", value: "group"))
+        }
+        
+        // Check if URL is valid
         guard let url = urlConstructor.url else { return }
         
+        //Run dataTask to get response from API
         let dataTask = session.dataTask(with: url) { (data, response, error) in
             if let data = data {
                 if let json = try? JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) {
@@ -39,5 +66,6 @@ class VKNetworkService {
         }
         dataTask.resume()
     }
+
 }
 
