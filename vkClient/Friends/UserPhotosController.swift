d888d01ead8969c90624f64496108e712583bee0
Anna Luchechko <annaluchechko@gmail.com>
2020-10-20 00:46:14 +0300

    Add UserPhotosController

---
+++ vkClient/Friends/UserPhotosController.swift
@@ -0,0 +1,9 @@
+//
+//  UserPhotosController.swift
+//  vkClient
+//
+//  Created by Anna Luchechko on 20.10.2020.
+//  Copyright © 2020 Anna Luchechko. All rights reserved.
+//
+
+import Foundation
